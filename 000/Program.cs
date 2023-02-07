58705fa350ee5ed93d2403a1a40c52f0896765f7
stels67 <melnikow.kirill@mail.ru>
2023-02-07 23:43:20 +0300

    Lesson1 001

---
+++ 000/Program.cs
@@ -0,0 +1,26 @@
+﻿/// с клавы вводится два числа. Найти их сумму 
+//String s;
+int a,b,sum;
+System.Console.Write("Введите a:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");
+b=Convert.ToInt32(Console.ReadLine());
+sum=a+b;
+//1. Конкотенация склеивания строк
+System.Console.WriteLine(a+"+"+b.ToString()+"="+Convert.ToString(sum));
+//2. Форматированный вывод
+System.Console.WriteLine("{0:F2}+{1:E5}={2:64}",a,b,sum);
+//3. Интрополяция 
+System.Console.WriteLine($"{a}+{b}={sum}");
+
+// С клавиатуры вводятся два числа a и b. Найти максимальное из них. (блок-схема)
+int max;
+if (a>b)
+{
+    max=a;
+}
+else
+{
+    max=b;
+}
+System.Console.WriteLine($"max={max}");
---
+++ 002/Program.cs
@@ -0,0 +1,14 @@
+﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго (блок-схема)
+
+double a,b;
+string s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a*a==b)
+    //System.Console.WriteLine("b является квадратом a");
+    System.Console.WriteLine("b является квадратом a");
+if (b*b==a)
+    System.Console.WriteLine("a является квадратом b");

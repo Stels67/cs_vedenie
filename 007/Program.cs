fede149bc0e9db24368a05bf02da86ff76831e98
stels67 <melnikow.kirill@mail.ru>
2023-02-08 00:41:23 +0300

    Lesson1 002

---
+++ 007/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным.
+
+int N2=10;
+bool odd= N2%2==0;
+if (odd)
+{
+    System.Console.WriteLine($"Число {N2} четное");
+}
+else 
+{
+    System.Console.WriteLine($"Число {N2} не четное");
+}

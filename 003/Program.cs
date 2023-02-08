8fe2773d0a1779781a7f62766415b4795a4bb523
stels67 <melnikow.kirill@mail.ru>
2023-02-08 21:18:29 +0300

    Lesson1 003

---
+++ 003/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них. (блок-схема)
+int a,b;
+
+int max;
+System.Console.Write("Введите a:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");
+b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    max=a;
+}
+else
+{
+    max=b;
+}
+System.Console.WriteLine($"max={max}");

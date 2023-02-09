008751405192e33543177910550f496d172dafca
stels67 <melnikow.kirill@mail.ru>
2023-02-09 23:57:50 +0300

    Lesson1 008

---
+++ 013/Program.cs
@@ -0,0 +1,8 @@
+﻿//Удалить вторую цифру целого числа введенного с клавиатуры.
+
+
+System.Console.WriteLine("Введтите число -");
+string a = Console.ReadLine();
+Console.WriteLine(a.Remove(1,1));
+
+

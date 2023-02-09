5a97cabe475df62acab011d2f618d3404e366117
stels67 <melnikow.kirill@mail.ru>
2023-02-10 01:58:49 +0300

    Lesson1 009

---
+++ 015/Program.cs
@@ -0,0 +1,13 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+int a, c;
+System.Console.WriteLine("Ввудите число -");
+a=Convert.ToInt32(Console.ReadLine());
+if(a>99)
+{
+    c=Math.Abs(a)/100;
+    System.Console.WriteLine(c);
+}
+else
+{
+    System.Console.WriteLine("No");
+}

75abf6d5747c621de0d69eecb1f49ab74a7e9093
stels67 <melnikow.kirill@mail.ru>
2023-02-09 22:49:22 +0300

    Lesson1 006

---
+++ 010/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+int a;
+int c;
+System.Console.WriteLine("введите число -");
+a=Convert.ToInt32(Console.ReadLine());
+c = Math.Abs(a)%10;
+System.Console.WriteLine(c);

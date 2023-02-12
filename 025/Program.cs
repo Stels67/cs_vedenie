abae1fb169fbf308f87f437d1fa8d56aa35ffc63
stels67 <melnikow.kirill@mail.ru>
2023-02-12 22:59:23 +0300

    Lesson2 025;

---
+++ 025/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+//(x>0 and y>0)
+
+int quarter= Convert.ToInt32(Console.ReadLine());
+if (quarter==1) System.Console.WriteLine("x>0 and y>0");
+if (quarter==2) System.Console.WriteLine("x<0 and y>0");
+if (quarter==3) System.Console.WriteLine("x<0 and y<0");
+if (quarter==4) System.Console.WriteLine("x<0 and y>0");

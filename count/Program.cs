92530a734be1fb55031dd965bee66c23b4d5ae74
stels67 <melnikow.kirill@mail.ru>
2023-02-12 22:12:15 +0300

    Lesson1 count;

---
+++ count/Program.cs
@@ -0,0 +1,19 @@
+﻿ // подсчитать колличество цифр и сумму цифр
+
+ int N=1234;
+ N=Convert.ToInt32(Console.ReadLine());
+ int counter=0; 
+ int sum=0;
+
+ while(N!=0)
+ {
+    //counter=counter+1;
+    counter++;
+    int d=N%10;
+    //sum=sum+d;
+    sum+=d;
+    //N=N/10;
+    N/=10;
+ }
+System.Console.WriteLine(counter);
+System.Console.WriteLine(sum);

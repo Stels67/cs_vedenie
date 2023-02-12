57f0f18f4093a137db40f2d803fc208c684752b8
stels67 <melnikow.kirill@mail.ru>
2023-02-12 22:22:17 +0300

    Lesson1 flag;

---
+++ Flag/Program.cs
@@ -0,0 +1,14 @@
+﻿// вводится число, проверить есть ли в этом числе цифра 0
+int N = 1234;
+bool flag=false;
+while(N!=0)
+{
+    int digit=N%10;
+    if (digit==0)
+        flag=true;
+    N/=10;
+}
+if (flag)
+    System.Console.WriteLine("В числе есть 0");
+else   
+    System.Console.WriteLine("В числе нет 0");

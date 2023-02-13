c3bd066998264742a232cff5ba2f1ae4c43b491c
stels67 <melnikow.kirill@mail.ru>
2023-02-13 23:36:24 +0300

    Lesson2 026;

---
+++ 026/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа проверяет пятизначное число на палиндромом. Например 12321
+
+string a;
+System.Console.WriteLine("Введите число = ");
+a=Console.ReadLine();
+char [] b = a.ToCharArray();
+Array.Reverse(b);
+string c = new string(b);
+if(a==c)
+{
+    System.Console.WriteLine("Палиндром");
+}
+if(a!=c)
+{
+    System.Console.WriteLine("не палиндром");
+}

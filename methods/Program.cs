02e7c911ba6980c9f83e207ed77bc66a0cc2de49
stels67 <melnikow.kirill@mail.ru>
2023-02-14 22:58:07 +0300

    Lesson3 math;

---
+++ methods/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите подпрограмму нахождения растояниия между двумя точками 
+
+double Distance(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y1-y2,2));
+}
+
+double x1=0,y1=0,x2=2,y2=2;
+
+//double Distance=Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y1-y2,2));
+double distance=Distance(0,0,2,2);
+System.Console.WriteLine(distance);

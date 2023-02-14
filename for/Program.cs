e868043c8e997b9b5295c2cea01280fd19add648
stels67 <melnikow.kirill@mail.ru>
2023-02-14 22:30:27 +0300

    Lesson2 foreach;

---
+++ for/Program.cs
@@ -0,0 +1,13 @@
+﻿// вывесьи на экран числа от 1 до 10(
+
+//int i=1;
+//while(i<=10);
+//{
+//    System.Console.WriteLine(i);
+//    i++;
+//}
+
+for(int i=1, k=100; i<=100 && k>=50; i++, k--)
+{
+    System.Console.WriteLine(i+ " "+k);
+}

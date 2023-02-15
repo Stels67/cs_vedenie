a2d5562f67879591c7d6496d50a1a471a2c41941
stels67 <melnikow.kirill@mail.ru>
2023-02-15 23:44:16 +0300

    Lesson3 031;

---
+++ 031/Program.cs
@@ -0,0 +1,15 @@
+﻿//31Вывести на экран кубы чисел от 1 до N
+
+Random random;
+random=new Random();
+int c;
+
+System.Console.WriteLine("Ввыедите какое колличество случайных цифр возведенных в куб вы хотите увидеть =");
+c=Convert.ToInt32(Console.ReadLine());
+for (int i=0; i<c; i++)
+
+{
+    int a=random.Next(1,100);
+    int b=(int)Math.Pow(a,3);
+    System.Console.WriteLine($"{a}, {b}"); 
+}

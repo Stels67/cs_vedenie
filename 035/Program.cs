aa28f433ce3dec1d1bbe9fb91e4aa383d57a9dee
stels67 <melnikow.kirill@mail.ru>
2023-02-16 01:36:56 +0300

    Lesson3 035;

---
+++ 035/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+int a;
+System.Console.WriteLine("введите число -");
+a=Convert.ToInt32(Console.ReadLine());
+if((a>1))
+{int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+            }
+Console.WriteLine(s);
+}

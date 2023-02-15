ce24f764767e3456982f142fc18d90fbbc3d1298
stels67 <melnikow.kirill@mail.ru>
2023-02-16 00:45:58 +0300

    Lesson3 032;

--- 033/Program.cs
+++ 033/Program.cs
@@ -1,6 +1,13 @@
 ﻿// Возведите число А в натуральную степень B используя цикл
 
-int a;
+int a,b;
 System.Console.WriteLine("Введите число а");
 a=Convert.ToInt32(Console.ReadLine());
-
+System.Console.WriteLine("Введите в какую степепень вы хотите возвести число=");
+b=Convert.ToInt32(Console.ReadLine());
+for (int i=0; i<1; i++)
+{
+    int c;
+    c=(int)Math.Pow(a,b);
+    System.Console.WriteLine(c);
+}

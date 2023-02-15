39ea7781d0bb9f227c5c69fe7d71495a39c50eeb
stels67 <melnikow.kirill@mail.ru>
2023-02-16 01:59:21 +0300

    Lesson3 035;

---
+++ 036/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+//int i=5;
+//int s=1;
+int a,b;
+System.Console.WriteLine("введите число -");
+a=Convert.ToInt32(Console.ReadLine());
+//while (i<10) 
+  //  {
+  //  if (i%2==0) 
+   // s*=i;
+   // i++;
+   // System.Console.WriteLine(i);
+   // }
+int result = 0;
+for (int i = 1; i <= b; i++)
+{
+	result += a;
+}
+Console.WriteLine(result);

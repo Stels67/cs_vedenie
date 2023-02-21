697ae24ec1024568ea463a145073fc68db2d56ff
stels67 <melnikow.kirill@mail.ru>
2023-02-21 23:18:00 +0300

    Lesson4.41

---
+++ 042/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] a=new int[12];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(-9,10);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i]}");
+
+System.Console.WriteLine();
+
+int sp=0;
+for(int i=0; i<a.Length;i++)
+    if (a[i]>0)
+        sp=sp+a[i];
+
+int sn=0;
+for(int i=0;1<a.Length;i++)
+    if (a[i]>0)
+        sp=sp+a[i];
+System.Console.WriteLine($"Sum of positive {sp}");
+System.Console.WriteLine($"Sum of negativ {sn}");

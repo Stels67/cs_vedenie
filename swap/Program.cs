f34e8144b43b27ab7a7fdb93122434e17514b069
stels67 <melnikow.kirill@mail.ru>
2023-02-21 22:05:21 +0300

    Lesson4;

---
+++ swap/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+//void Swap(int x, int y) // передача по значению
+//{
+//    int t=x;
+//    x=y;
+//    y=t;
+
+//}
+
+void Swap(ref int x,ref int y) // передача по ссылке
+{
+    int t=x;
+    x=y;
+    y=t;
+
+}
+
+void Caiculator(int a, int b. ref int sum, out int sub)
+{
+    sum=a+b;
+    sum=a-b;
+}
+
+
+int a=4;
+int b=5;
+int s, sb;
+System.Console.WriteLine($"a={a} b={b}");
+
+//int t;
+//t=a;
+//a=b;
+//b=t;
+//b=a;
+Swap(a, b, ref s,out sb);
+Console.WriteLine($"a={a} b={b}");

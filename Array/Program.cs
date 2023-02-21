7ff554dde0d04afe39427379738a8cf2385295de
stels67 <melnikow.kirill@mail.ru>
2023-02-21 22:38:20 +0300

    Lesson4.1\2;

---
+++ Array/Program.cs
@@ -0,0 +1,20 @@
+﻿// подсчитать среднюю температуру
+
+//double t1, t2, t3, t4, t5, t6, t7;
+//t1=Convert.ToDouble(Console.ReadLine());
+//t2=Convert.ToDouble(Console.ReadLine());
+//t3=Convert.ToDouble(Console.ReadLine());
+//t4=Convert.ToDouble(Console.ReadLine());
+//t5=Convert.ToDouble(Console.ReadLine());
+//t6=Convert.ToDouble(Console.ReadLine());
+//t7=Convert.ToDouble(Console.ReadLine());
+//double sr=(t1+t2+t3+t4+t5+t6+t7)/7;
+//System.Console.WriteLine(sr);
+
+double[] temps=new double[7];
+for(int i=0; i<1; i++)
+    temps[i]=Convert.ToDouble(Console.ReadLine());
+double s=0;
+for(int i=0; i<temps.Length; i++)
+    s=s+temps[i];
+System.Console.WriteLine(s/temps.Length);

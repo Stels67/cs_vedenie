4e075e9b1da7ff47a83eb09f40975fb17ab5faa7
stels67 <melnikow.kirill@mail.ru>
2023-02-25 16:34:22 +0300

    Lesson4.46

--- 046/Program.cs
+++ 046/Program.cs
@@ -1,8 +1,17 @@
 ﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+    
 int[] a=new int[123];
 Random random=new Random();
 for(int i=0;i<a.Length;i++)
-    a[i]=random.Next(0,1001);
+    a[i]=random.Next(0,1010);
 for(int i=0;i<a.Length;i++)
-    System.Console.Write($"{a[i]}");
-    
+   System.Console.Write($"{a[i]} ");
+
+int g=a[0];
+int count=0;
+for(int i=0;i<a.Length;i++)
+    if((a[i]>10) && (a[i]<100)) count++;
+    {
+        System.Console.WriteLine($"Элементов из диапозона от 10 до 99 = {count}");
+    }

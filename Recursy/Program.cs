9bbb4c9924760f2d4ecd17e0b33b7135763044c1
stels67 <melnikow.kirill@mail.ru>
2023-03-11 18:42:40 +0300

    Lesson7.1

--- Recursy/Program.cs
+++ Recursy/Program.cs
@@ -2,7 +2,7 @@
 
 //int N=10;
 
-//int i=0;
+int i=0;
 //while(i<N)
 //{
     //i++;
@@ -18,4 +18,22 @@ void Loop(int i, int N)
     System.Console.Write($"{1,4}");
     if (1<N) Loop(i+1,N);
 }
-Loop(1,20);
+//Loop(1,20);
+
+int F(int N)
+{
+    if (N>0) return F(N-1)*N;
+    else return 1;
+}
+//System.Console.WriteLine(F(5));
+
+int Fib(int N)
+{
+        if (N==0) return 0;
+            else 
+                if (N==1) return 1;
+                    else
+                        return Fib(N-1)+Fib(N-2);
+}
+for(i=0;i<10;i++)
+    System.Console.WriteLine(Fib(i));

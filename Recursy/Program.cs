b7befe7c4b0ff6fba85be021bdcd804889d728a8
stels67 <melnikow.kirill@mail.ru>
2023-03-11 17:49:33 +0300

    Lesson7

---
+++ Recursy/Program.cs
@@ -0,0 +1,21 @@
+﻿//  
+
+//int N=10;
+
+//int i=0;
+//while(i<N)
+//{
+    //i++;
+//    System.Console.Write($"{i++,4}");
+//}
+//metka:
+//    System.Console.Write($"{i++,4}");
+//if (int>N) goto metka
+
+
+void Loop(int i, int N)
+{
+    System.Console.Write($"{1,4}");
+    if (1<N) Loop(i+1,N);
+}
+Loop(1,20);

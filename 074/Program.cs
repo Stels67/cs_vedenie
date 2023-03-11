f790e294b4bec117ec2f094db382d7ad4dea4663
stels67 <melnikow.kirill@mail.ru>
2023-03-11 16:33:36 +0300

    Lesson74

---
+++ 074/Program.cs
@@ -0,0 +1,41 @@
+﻿// 74. Найти сумму цифр числа
+Console.WriteLine("Программа выводит сумму чисел введенного числа");
+System.Console.Write("Задайте число. "); // по условию N надо задать 
+int N = InputN();
+  int sum=0;
+System.Console.WriteLine(SumDigitsFromNumbers(N));
+
+
+
+int InputN()
+{
+  int number = -1;
+  bool ErrorStatus = false;
+  do
+  {
+    System.Console.WriteLine("Введите целое число:");
+    try
+    {
+      number = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+      System.Console.WriteLine("Ошибка. Введите целое число");
+      ErrorStatus = true;
+    }
+  } while (ErrorStatus==true);
+  return number;
+}
+
+
+int SumDigitsFromNumbers (int num)
+{
+  if (num==0) 
+  {
+  return 0;
+  }
+  else 
+  {
+    return (SumDigitsFromNumbers(num/10)) + num%10;
+  }
+} 

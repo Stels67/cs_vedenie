8423f22dd779fd7f791e57921d113302d5c45cab
stels67 <melnikow.kirill@mail.ru>
2023-03-12 21:58:37 +0300

    Lesson86

---
+++ 086/Program.cs
@@ -0,0 +1,14 @@
+﻿// // 86. Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
+System.Console.WriteLine("Введите текст:");
+string? inputData = Console.ReadLine();
+System.Console.WriteLine("Введите символ, который нужно сосчитать:");
+ConsoleKeyInfo keyInfo;
+keyInfo = Console.ReadKey();
+char soughtForSymbol = keyInfo.KeyChar;
+int counter = 0;
+foreach (char Symbol in inputData)
+{
+  if (Symbol == soughtForSymbol) counter++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"Символ \"{soughtForSymbol}\" встретился {counter} раз(а).");

b7cf66d70ff3c3e4cc715bba9a303330968f7bf7
stels67 <melnikow.kirill@mail.ru>
2023-03-12 22:03:37 +0300

    Lesson88

---
+++ 088/Program.cs
@@ -0,0 +1,23 @@
+﻿// // 88. Заменить все вхождения подстроки w в строке st на строку v. 
+// Вывести на экран первоначальную строку и конечную строку
+
+string a = "Начало текста.";
+string b = "Середина текста.";
+string w = "Конец текста.";
+
+string [] array = new string [] {a , b , w}; 
+
+string result = string.Join(" ", array);
+
+System.Console.WriteLine(result);
+
+string v = "Не конец, а окончание!";
+
+for (int i = 0; i<array.Length; i++) // на случай масштабирования
+  if (array[i] == w) array[i] = v;
+
+string result2 = string.Join(" ", array);
+
+System.Console.WriteLine(result2);
+
+// Так понял условие. Уж очень обобщенно (или запутано) написано

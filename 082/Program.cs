15ab6941de952f08000684b24535d6ad5627c8dd
stels67 <melnikow.kirill@mail.ru>
2023-03-12 21:53:13 +0300

    Lesson82

---
+++ 082/Program.cs
@@ -0,0 +1,5 @@
+﻿// 082  C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+System.Console.WriteLine("Введите текст. Программа посчитает кол-во символов до первой точки");
+string? InputText = Console.ReadLine ();
+int SymbolsBeforeDot = InputText.IndexOf('.');
+System.Console.WriteLine($"{SymbolsBeforeDot}");
